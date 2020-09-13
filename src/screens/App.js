bcdb334363effb4c0bd3107cb9b3b98f7022f8dc
Megha Makvana <meghamakvana96@gmail.com>
2020-09-14 04:48:13 +0530

    add App.js

---
+++ src/screens/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from "./login/Login";
+import Home from "./home/Home";
+
+class App extends Component {
+  baseUrl = "https://api.instagram.com/v1/users/self/";
+
+  render() {
+    return (
+      <Router>
+        <div className="main-container">
+          <Route
+            exact
+            path="/"
+            render={(props) => <Login {...props} baseUrl={this.baseUrl} />}
+          />
+          <Route
+            exact
+            path="/home"
+            render={(props) => <Home {...props} baseUrl={this.baseUrl} />}
+          />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
